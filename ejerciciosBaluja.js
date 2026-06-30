b5aef26a4bb3a473e556b6b7d48554729c6fbd4d
DaBal1991 <103607724+DaBal1991@users.noreply.github.com>
2026-06-30 10:46:14 -0300

    Add files via upload

---
+++ ejerciciosBaluja.js
@@ -0,0 +1,72 @@
+/*
+    Resolvé las 10 funciones declaradas en este archivo con el menor código posible, aprovechando las ventajas de las funciones de orden superior y la sintaxis de ES6 con JavaScript.
+
+    En el archivo 'data.js' se encuentra una variable global que contiene un array de países sudamericanos. Tené en cuenta la estructura de cada objeto país para resolver lo pedido.
+*/
+
+// Función de ejemplo, operando sobre la variable 'data' declarada en el archivo homónimo
+const cantPaises = (dataJson) => data.length;
+const poblMundial = 8300000000;
+
+// 1
+const imprimirPaises = (dataJson) => {
+    /* Debe imprimir la info de todos los países (solo su nombre común y su capital) */
+    data.forEach(pais => {
+        return console.log(`Nombre común: ${pais.name.common} - Capital: ${pais.capital[0]}`)
+    });
+}
+
+// 2
+const nombresDePaises = (dataJson) => {
+    /* Debe retornar un array con los nombres oficiales de cada país */
+    return nombresOficiales = data.map(pais => pais.name.official);
+}
+
+// 3
+const nombresDeCapitales = (dataJson) => {
+    /* Debe retornar un array con los nombres de las capitales de cada país */
+    return nombresCapitales = data.map(pais => pais.capital);
+}
+
+// 4
+const poblacionTotal = (dataJson) => {
+    /* Debe retornar la población total de Sudamérica */
+    return poblacionSudamerica = data.reduce((acumulador, pais) => {acumulador + pais.population}, 0);
+}
+
+// 5
+const poblacionTotalMundialSinSud = (poblMundial) => {
+    /* Debe retornar la población total mundial (llega por parámetro) sin reutilizar la función 'poblacionTotal', más bien restando sucesivamente la población de cada país a la población mundial */
+    return poblacionSinSud = data.reduce((acumulador, pais) => {acumulador - pais.population}, poblMundial);
+}
+
+// 6
+const todosEmpiezanLosLunes = (dataJson) => {
+    /* Debe retornar si todos los países inician la semana los días lunes (monday) */
+    return arrancanLunes = data.every(pais => pais.startOfWeek === "monday");
+}
+
+// 7
+const algunoNoConducePorLaDerecha = (dataJson) => {
+    /* Debe retornar si en alguno de los países no se conduce por la derecha (right) */
+    return conduceNoDerecha = data.some(pais => pais.car.side !== "right");
+}
+
+// 8
+const cualesNoConducenPorLaDerecha = (dataJson) => {
+    /* Debe retornar a los países donde no se conduce por la derecha (right) */
+    return noConducenDerecha = data.filter(pais => pais.car.side !== "right");
+}
+
+// 9
+const cualesNoConducenPorLaDerechaSimpl = (dataJson) => {
+    /* Debe retornar solo los nombres comunes de los países donde no se conduce por la derecha (right) */
+    return nombresNoConducenDerecha = data.filter(pais => pais.car.side !== "right").map(pais => pais.name.common);
+}
+
+// 10
+const paisesQueLimitanConArgentina = (dataJson) => {
+    /* Debe retornar solo los nombres comunes de los países que limitan con Argentina */
+    /* WARNING: Hay países que no tienen esa info. A tenerlo en cuenta para evitar errores */
+    return limitanArgentina = data.filter(pais => pais.borders?.includes("ARG")).map(pais => pais.name.common);
+}
